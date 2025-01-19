0183a90ad0de029e0a2c40329cd9be13d1bacc21
Dhinagar D <117499917+Dhinagar27@users.noreply.github.com>
2025-01-19 19:16:49 +0530

    Add files via upload

---
+++ JS/Functions/demo.js
@@ -0,0 +1,12 @@
+var factor = "Vijay"
+var fplayer = "Dhoni"
+var fmovie = "Intersteller"
+
+function fav() {
+
+    console.log("Favourite Actor  : " + factor)
+    console.log("Favourite Player : " + fplayer)
+    console.log("Favourite Movie  : " + fmovie)
+}
+
+fav()
